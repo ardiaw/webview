136ac13629a2959476b3f838cb392dfb2306c524
ardiaw <ardialintang526@gmail.com>
2018-05-07 19:29:16 +0700

    v1.0.0

---
+++ app/src/main/java/com/example/admin/webview/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.admin.webview;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    SwipeRefreshLayout swipeRefreshLayout;
+    WebView webView;
+    WebSettings webSettings;
+
+    String URL = "http://www.youtube.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        webView = (WebView) findViewById(R.id.web_view);
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.getUseWideViewPort();
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                swipeRefreshLayout.setRefreshing(true);
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        webView.loadUrl(URL);
+
+    }
+
+    @Override
+    public void onRefresh() {
+        webView.reload();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            finish();
+            System.exit(0);
+        }
+    }
+}
